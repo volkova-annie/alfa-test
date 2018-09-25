1d0db066c3db7bfc67444b1a01714471fa2a81c4
Volkova Annie <annie_v@bk.ru>
2018-09-25 11:28:23 +0300

    Replace tabs on spaces

--- getAnagram.js
+++ getAnagram.js
@@ -1,49 +1,49 @@
 const str = 'адрес карп кума куст мир мука парк среда стук рост сорт трос';
 
 function getAnagrams(str) {
-	let result = [];
+  let result = [];
 
-	if (!str)
-		return result;
+  if (!str)
+    return result;
 
-	let wordsOrigArr = str.split(' ');
+  let wordsOrigArr = str.split(' ');
 
-	if (wordsOrigArr.length <= 1) {
-		return result;
-	}
+  if (wordsOrigArr.length <= 1) {
+    return result;
+  }
 
-	let wordsArr = wordsOrigArr.map((value, key) => {
-		const val = value.split('').sort().join('').toLowerCase(); // sort by letters  карп ->  акрп // ['a', 'к', 'р', 'п']
-		return {index: key, word: val};
-	}) //[{},{}]
+  let wordsArr = wordsOrigArr.map((value, key) => {
+    const val = value.split('').sort().join('').toLowerCase(); // sort by letters  карп ->  акрп // ['a', 'к', 'р', 'п']
+    return {index: key, word: val};
+  }) //[{},{}]
 
-	wordsArr.sort((a, b) => {
-		let compareByLength = a.word.length - b.word.length;
-		return compareByLength == 0 // same length
-			? a.word.localeCompare(b.word)
-			: compareByLength
-		}); // sort by length then lexigraphicaly
+  wordsArr.sort((a, b) => {
+    let compareByLength = a.word.length - b.word.length;
+    return compareByLength == 0 // same length
+      ? a.word.localeCompare(b.word)
+      : compareByLength
+    }); // sort by length then lexigraphicaly
 
-	let currAnagrams = wordsOrigArr[wordsArr[0].index]; // store original word
-	let notEmpty = false;
-	for (let i = 1; i < wordsArr.length; i++) {
-		if (wordsArr[i].word ===  wordsArr[i - 1].word) { // compare current with prev
-			currAnagrams += '-' + wordsOrigArr[wordsArr[i].index]; // store original word
-			notEmpty = true;
-		} else {
-			if (notEmpty) {
-				result.push(currAnagrams);
-			}
-			currAnagrams = wordsOrigArr[wordsArr[i].index]; // store original work
-			notEmpty = false;
-		}
-	}
+  let currAnagrams = wordsOrigArr[wordsArr[0].index]; // store original word
+  let notEmpty = false;
+  for (let i = 1; i < wordsArr.length; i++) {
+    if (wordsArr[i].word ===  wordsArr[i - 1].word) { // compare current with prev
+      currAnagrams += '-' + wordsOrigArr[wordsArr[i].index]; // store original word
+      notEmpty = true;
+    } else {
+      if (notEmpty) {
+        result.push(currAnagrams);
+      }
+      currAnagrams = wordsOrigArr[wordsArr[i].index]; // store original work
+      notEmpty = false;
+    }
+  }
 
-	if (notEmpty) {
-		result.push(currAnagrams);
-	}
+  if (notEmpty) {
+    result.push(currAnagrams);
+  }
 
-	return result;
+  return result;
 }
 
 getAnagrams(str);
